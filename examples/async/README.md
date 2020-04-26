41b68baf90817b85733492a835d59ffd268f25f1
Pauan <pauanyu+github@pm.me>
2020-04-26 05:24:34 +0200

    Adding in livereload to the examples

--- examples/async/README.md
+++ examples/async/README.md
@@ -7,6 +7,14 @@ yarn install
 ## How to build
 
 ```sh
+# Builds the project and opens it in your browser.
+# It will auto-reload when you make any changes.
+yarn start
+```
+
+## How to build for production
+
+```sh
 # Builds the project and places it into the `dist` folder.
 yarn run build
 ```
--- examples/todomvc/rollup.config.js
+++ examples/todomvc/rollup.config.js
@@ -1,4 +1,8 @@
 import rust from "@wasm-tool/rollup-plugin-rust";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+
+const is_watch = !!process.env.ROLLUP_WATCH;
 
 export default {
     input: {
@@ -12,6 +16,14 @@ export default {
     plugins: [
         rust({
             serverPath: "js/",
+            debug: false,
         }),
+
+        is_watch && serve({
+            contentBase: "dist",
+            open: true,
+        }),
+
+        is_watch && livereload("dist"),
     ],
 };
