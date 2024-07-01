c7f31e0469b305e1b1b41eb0da169115960ac26b
Pauan <pauanyu+github@pm.me>
2024-06-30 19:08:07 -0700

    Fixing minor typo

--- examples/counter/src/lib.rs
+++ examples/counter/src/lib.rs
@@ -82,7 +82,6 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
     let app = App::new();
     dominator::append_dom(&dominator::body(), App::render(&app));
 
